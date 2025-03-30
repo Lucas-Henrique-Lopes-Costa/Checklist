d28e5e856f3b8701eda9cdae9e55cb0c412a754b
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2025-03-30 11:48:48 -0300

    upload

---
+++ Checklist/ChecklistApp.swift
@@ -0,0 +1,32 @@
+//
+//  ChecklistApp.swift
+//  Checklist
+//
+//  Created by Lucas Henrique on 30/03/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ChecklistApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ChecklistTests/ChecklistTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChecklistTests.swift
+//  ChecklistTests
+//
+//  Created by Lucas Henrique on 30/03/25.
+//
+
+import Testing
+
+struct ChecklistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
